4697cb06c3766ac9bb24bf48db5715441808e1e4
Andrew Skow <anjeie24@mail.ru>
2025-10-22 14:14:44 +0300

    newspaper app

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("pages.urls")),
+    path("articles/", include("articles.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),

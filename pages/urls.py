16d703fd9f8b887bafc81df495ed32ef655ae7ba
Andrew Skow <anjeie24@mail.ru>
2025-10-22 11:05:36 +0300

    Add env_example and remode .idea

--- pages/urls.py
+++ pages/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, test_email
 
-urlpatterns = [path("", HomePageView.as_view(), name="home")]
+urlpatterns = [
+    path("", HomePageView.as_view(), name="home"),
+    path("test-email/", test_email, name="test_email"),
+]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,21 @@
+from django.conf import settings
 from django.views.generic import TemplateView
+from django.core.mail import send_mail
+from django.http import HttpResponse
 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+
+def test_email(request):
+    send_mail(
+        "Тема рассылки",
+        "Сообщение письма",
+        "andrewskow@yandex.ru",
+        [
+            "anjeie24@mail.ru",
+        ],
+        fail_silently=False,
+    )
+    return HttpResponse("Message was send")
